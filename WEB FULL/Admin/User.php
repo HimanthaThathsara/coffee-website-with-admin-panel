4900fc378da91b73c582897dca55f6eab146cec9
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-05-05 21:04:45 +0530

    Create User.php

---
+++ WEB FULL/Admin/User.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+if (!isset($_SESSION['auth']) || $_SESSION['auth'] !== true) {
+    header('Location: Login.php');
+    exit();
+}
+?>
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>User Management</title>
+    <link href="assets/vendor/fontawesome/css/fontawesome.min.css" rel="stylesheet">
+    <link href="assets/vendor/fontawesome/css/solid.min.css" rel="stylesheet">
+    <link href="assets/vendor/fontawesome/css/brands.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/DataTables/datatables.min.css" rel="stylesheet">
+    <link href="assets/css/master.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="wrapper">
+        <!-- sidebar navigation component -->
+        <?php include ('PHPcomponent/SideBar.php') ?>
+
+        <div id="body" class="active">
+            <!-- navbar navigation component -->
+            <?php include('PHPcomponent/Navbar.php') ?>
+            <!-- Role Table component -->
+            <?php include('PHPcomponent/User.php') ?>
+        </div>
+    </div>
+
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/datatables/datatables.min.js"></script>
+    <script src="assets/js/script.js"></script>
+    
+</body>
+
+</html>

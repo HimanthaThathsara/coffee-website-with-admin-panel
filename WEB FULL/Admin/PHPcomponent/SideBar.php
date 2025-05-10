6f4a45d072f290289aacb5c5290f9388bc98b76e
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-05-11 00:55:32 +0530

    Create SideBar.php

---
+++ WEB FULL/Admin/PHPcomponent/SideBar.php
@@ -0,0 +1,26 @@
+
+<nav id="sidebar" class="active">
+
+            <ul class="list-unstyled components text-secondary">
+                <li>
+                    <a href="AdminIndex.php"><i class="fas fa-home"></i> Dashboard</a>
+                </li>
+                <li>
+                    <a href="#authmenu" data-bs-toggle="collapse" aria-expanded="false" class="dropdown-toggle no-caret-down"><i class="fas fa-user-shield"></i> Authentication</a>
+                    <ul class="collapse list-unstyled" id="authmenu">
+                        <li>
+                            <a href="roles.php"><i class="fas fa-lock"></i> Admins</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="Product.php"><i class="fas fa-tv"></i>Products</a>
+                </li>
+                <li>
+                    <a href="User.php"><i class="fas fa-user-friends"></i>Users</a>
+                </li>
+                <li>
+                    <a href="#"><i class="fas fa-cog"></i>Settings</a>
+                </li>
+            </ul>
+        </nav>

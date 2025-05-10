c299b06dd042e5c14aeab7bce17c10a6df9d07f0
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-05-11 00:55:21 +0530

    Create User.php

---
+++ WEB FULL/Admin/PHPcomponent/User.php
@@ -0,0 +1,42 @@
+
+<div class="content">
+    <div class="container">
+    <div class="content card">
+                        <div class="container">
+                            <div class="page-title">
+                                <h3>User Management
+                                </h3>
+                            </div>
+                            <div class="box box-primary">
+                                <div class="box-body">
+                                    <table width="100%" class="table table-hover" id="dataTables-example">
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Email</th>
+                                                <th>Password</th>
+                                                <th>Address</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Oshada</td>
+                                                <td>Oshdarathnayak@gmail.com</td>
+                                                <td>Nothing123</td>
+                                                <td>277 
+                                                    Badulla 
+                                                    sri-lanaka</td>
+                                                <td class="text-end">
+                                                    <a href="" class="btn btn-outline-danger btn-rounded"><i class="fas fa-trash"></i></a>
+                                                </td>
+                                            </tr>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    </div>
+</div>    

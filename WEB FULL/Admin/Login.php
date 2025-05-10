600dfc1ea34fa43ad970cfaf9266a3559e4e05cc
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-05-11 00:55:44 +0530

    Create Login.php

---
+++ WEB FULL/Admin/Login.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Login Admin</title>
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/auth.css" rel="stylesheet">
+</head>
+<body>
+    <!--Login Form-->
+    <?php include('PHPcomponent/Login.php')?>
+    
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
